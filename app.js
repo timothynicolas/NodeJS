9228536f602e5ee43b836d3c46699a88dcf60da0
Timothy Nicolas <timothynicolas24@gmail.com>
2026-07-03 21:32:42 +0800

    finished lesson 1-2

---
+++ app.js
@@ -0,0 +1,10 @@
+// CommonJS, every file is a module (by default)
+// MOddules - Encapsulated code (only share minimum)
+
+const john = 'john';
+
+const sayHi = (name) => {
+  console.log(`Hello there, ${name}`);
+}
+
+sayHi(john);
