6d78e081d14df1315ffadb4c2a87c3a5a1ef002d
Timothy Nicolas <timothynicolas24@gmail.com>
2026-08-05 15:32:27 +0800

    finished DELETE exercise 11.4

---
+++ 2-express-tutorial/exercises/11.4/app.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+const { valorantAgents } = require("./data");
+
+app.use(express.json());
+
+app.put("/api/agents/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  if (!name) {
+    return res.status(400).send("Please provide name");
+  }
+
+  const agent = valorantAgents.find((agent) => agent.id === Number(id));
+
+  if (!agent) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no agent with id ${id}` });
+  }
+
+  const newAgentsList = valorantAgents.map((agent) => {
+    if (agent.id === Number(id)) {
+      return { ...agent, name };
+    }
+
+    return agent;
+  });
+
+  res.status(200).json({ success: true, data: newAgentsList });
+});
+
+app.delete("/api/agents/:id", (req, res) => {
+  const { id } = req.params;
+  const agent = valorantAgents.find((agent) => agent.id === Number(id));
+  if (!agent) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `no agent with id ${id}` });
+  }
+
+  const newAgentsList = valorantAgents.filter(
+    (agent) => agent.id !== Number(id),
+  );
+
+  res.status(200).send({ success: true, data: newAgentsList });
+});
+
+app.listen(5000, () => {
+  console.log("server is listening on port 5000...");
+});
---
+++ 2-express-tutorial/exercises/11.4/data.js
@@ -0,0 +1,24 @@
+const valorantAgents = [
+  {
+    id: 1,
+    name: "Jett",
+    role: "Duelist",
+  },
+  {
+    id: 2,
+    name: "Chamber",
+    role: "Sentinel",
+  },
+  {
+    id: 3,
+    name: "Reyna",
+    role: "Duelist",
+  },
+  {
+    id: 4,
+    name: "Omen",
+    role: "Controller",
+  },
+];
+
+module.exports = { valorantAgents };
